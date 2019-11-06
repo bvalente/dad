bea3e5f26cbe554330d5d1423436aca01ba958cb
Bernardo Valente <bernardofvalente@gmail.com>
2019-11-06 16:14:37 +0000

    puppetMaster ui done

--- msdad/puppetMaster/MainWindow.xaml.cs
+++ msdad/puppetMaster/MainWindow.xaml.cs
@@ -23,7 +23,37 @@ namespace puppetMaster{
         StackPanel clientPanel;
         StackPanel serverPanel;
         TextBox puppetMasterScript;
-        Button puppetMasterScriptButton;
+
+        //CreateSeverPanel
+        TextBox createServerID; 
+        TextBox createServer_url;
+        TextBox createServer_max_faults;
+        TextBox createServer_min_delay;
+        TextBox createServer_max_delay;
+
+        //CreateClientPanel
+        TextBox createClient_username;
+        TextBox createClient_client_url;
+        TextBox createClient_server_url;
+        TextBox createClient_script_file;
+
+        //AddRoomPanel
+        TextBox room_location;
+        TextBox room_capacity;
+        TextBox room_name;
+
+        //CrashPanel
+        TextBox crashServerID;
+
+        //FreezePanel
+        TextBox freezeServerID;
+
+        //UnfreezePanel
+        TextBox unfreezeServerID;
+        
+        //WaitPanel
+        TextBox waitTimeBox;
+        
         //unused
         StackPanel pcsPanel;
         TextBox pcsIp;
@@ -36,14 +66,54 @@ namespace puppetMaster{
             InitializeComponent();
             
             //load all necessary components
-            clientPanel = this.Find<StackPanel>("ClientPanel");
+            clientPanel = this.Find<StackPanel>("ClientPanelList");
             Console.WriteLine("Loading: " + clientPanel.Name);
-            serverPanel = this.Find<StackPanel>("ServerPanel");
+            serverPanel = this.Find<StackPanel>("ServerPanelList");
             Console.WriteLine("Loading: " + serverPanel.Name);
+            
             puppetMasterScript = this.Find<TextBox>("PuppetMasterScript");
             Console.WriteLine("Loading: " + puppetMasterScript.Name);
-            puppetMasterScriptButton = this.Find<Button>("PuppetMasterScriptButton");
-            Console.WriteLine("Loading: " + puppetMasterScriptButton.Name);
+
+            createServerID = this.Find<TextBox>("createServerID");
+            Console.WriteLine("Loading: " + createServerID.Name);
+            createServer_url = this.Find<TextBox>("createServer_url");
+            Console.WriteLine("Loading: " + createServer_url.Name);
+            createServer_max_faults = this.Find<TextBox>("createServer_max_faults");
+            Console.WriteLine("Loading: " + createServer_max_faults.Name);
+            createServer_min_delay = this.Find<TextBox>("createServer_min_delay");
+            Console.WriteLine("Loading: " + createServer_min_delay.Name);
+            createServer_max_delay = this.Find<TextBox>("createServer_max_delay");
+            Console.WriteLine("Loading: " + createServer_max_delay.Name);
+            
+               
+            createClient_username = this.Find<TextBox>("createClient_username");
+            Console.WriteLine("Loading: " + createClient_username.Name);
+            createClient_client_url = this.Find<TextBox>("createClient_client_url");
+            Console.WriteLine("Loading: " + createClient_client_url.Name);
+            createClient_server_url = this.Find<TextBox>("createClient_server_url");
+            Console.WriteLine("Loading: " + createClient_server_url.Name);
+            createClient_script_file = this.Find<TextBox>("createClient_script_file");
+            Console.WriteLine("Loading: " + createClient_script_file.Name);
+            
+            room_location = this.Find<TextBox>("room_location");
+            Console.WriteLine("Loading: " + room_location.Name);
+            room_capacity = this.Find<TextBox>("room_capacity");
+            Console.WriteLine("Loading: " + room_capacity.Name);
+            room_name = this.Find<TextBox>("room_name");
+            Console.WriteLine("Loading: " + room_name.Name);
+            
+            crashServerID = this.Find<TextBox>("CrashServerID");
+            Console.WriteLine("Loading: " + crashServerID.Name);
+
+            freezeServerID = this.Find<TextBox>("FreezeServerID");
+            Console.WriteLine("Loading: " + freezeServerID.Name);
+
+            unfreezeServerID = this.Find<TextBox>("UnfreezeServerID");
+            Console.WriteLine("Loading: " + unfreezeServerID.Name);
+            
+            waitTimeBox = this.Find<TextBox>("WaitTime");
+            Console.WriteLine("Loading: " + waitTimeBox.Name);
+
             
             puppetMaster = new PuppetMaster();//TODO make it singleton
 
@@ -75,6 +145,64 @@ namespace puppetMaster{
             foreach (string command in commands){
                 puppetMaster.executeCommand(command);
             }
+        }
+
+        public void createServer(object sender, RoutedEventArgs e){
+
+            string server_id = createServerID.Text; 
+            string url =  createServer_url.Text;
+            string max_faults = createServer_max_faults.Text;
+            string min_delay = createServer_min_delay.Text;
+            string max_delay =  createServer_max_delay.Text;
+
+            puppetMaster.createServer(server_id, url, max_faults, min_delay, max_delay);
+        }
+
+        public void createClient(object sender, RoutedEventArgs e){
+
+            string username = createClient_username.Text;
+            string url = createClient_client_url.Text;
+            string server_url = createClient_server_url.Text;
+            string script_file = createClient_script_file.Text;
+
+            puppetMaster.createClient(username, url, server_url, script_file);
+        }
+
+        public void addRoom(object sender, RoutedEventArgs e){
+            
+            string location = room_location.Text;
+            string capacity = room_capacity.Text;
+            string name = room_name.Text;
+            
+            puppetMaster.addRoom(location, capacity, name);
+        }
+
+
+        public void status(object sender, RoutedEventArgs e){
+            
+            puppetMaster.status();
+        }
+
+        public void crash(object sender, RoutedEventArgs e){
+            string server_id = crashServerID.Text;
+        }
+
+        public void freezeServer(object sender, RoutedEventArgs e){
+            
+            string server_id = freezeServerID.Text;
+            puppetMaster.freezeServer(server_id);
+        }
+
+        public void unfreezeServer(object sender, RoutedEventArgs e){
+            
+            string server_id = unfreezeServerID.Text;
+            puppetMaster.unfreezeServer(server_id);
+        }
+
+        public void waitTime(object sender, RoutedEventArgs e){
+
+            string time = waitTimeBox.Text;
+            puppetMaster.wait(time);
         }
 
         //NOT IN USE
@@ -172,7 +300,7 @@ namespace puppetMaster{
 
         public void createServer(object pcs){
             //TODO
-            return;
+            //return;
         }
 
     }
